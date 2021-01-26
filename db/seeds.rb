bddde89a091d17191506d6479a88e4ab518de7ca
jmart6784 <jmartinez6784@yahoo.com>
2021-01-25 22:22:05 -0800

    Added and styled contact section

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 
 # Project.create(image: "cart.png", title: "Shopping Cart", description: "A shopping cart React app that uses React router", tags: "React, SCSS", live: "https://jmart6784.github.io/shopping-cart/", github: "https://github.com/jmart6784/shopping-cart")
 
-# Project.create(image: "booker.png", title: "Odin Flight Booker", description: "A Flight booking app made using Rails", tags: "Rails", live: "https://odin-flight-booker365.herokuapp.com/", github: "https://github.com/jmart6784/odin-flight-booker")
+# Project.create(image: "booker.png", title: "Odin Flight Booker", description: "A Flight booking app made using Rails", tags: "Rails, CSS", live: "https://odin-flight-booker365.herokuapp.com/", github: "https://github.com/jmart6784/odin-flight-booker")
 
 # Project.create(image: "instagram.png", title: "Instagram clone", description: "A full-stack Instagram clone app that uses Rails as the back-end. This project is also mobile friendly.", tags: "Rails, Javascipt, SCSS", live: "Soon", github: "https://github.com/jmart6784/instagram")
 

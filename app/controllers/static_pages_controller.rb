5a29fe8de1d5caf1356556a0bd6030d172e89e63
jmart6784 <jmartinez6784@yahoo.com>
2021-01-28 23:47:52 -0800

    Project section contains pre-picked projects

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,19 @@
 class StaticPagesController < ApplicationController
   def index
-    
+    @projects = [
+      Project.find_by(title: "Instagram clone"),
+      Project.find_by(title: "Odin Book"),
+      Project.find_by(title: "Where's Waldo?"),
+      Project.find_by(title: "Shopping Cart"),
+      Project.find_by(title: "Battleship"),
+      Project.find_by(title: "Memory Card"),
+      Project.find_by(title: "Odin Weather"),
+      Project.find_by(title: "Flickr Feed"),
+      Project.find_by(title: "Odin Kittens"),
+      Project.find_by(title: "Odin Library"),
+      Project.find_by(title: "Tic Tac Toe"),
+      Project.find_by(title: "CSS grid Framework")
+    ]
   end
 
   def projects
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,57 +1,55 @@
-# Project.create(image: "waldo.png", title: "Where's Waldo?", description: "Character finding game built with rails as the back-end", tags: "Rails, Javascript, SCSS", live: "https://odin-wheres-waldo-app.herokuapp.com/", github: "https://github.com/jmart6784/waldo")
+Project.create(image: "waldo.png", title: "Where's Waldo?", description: "Character finding game built with rails as the back-end", tags: "Rails, Javascript, SCSS", live: "https://odin-wheres-waldo-app.herokuapp.com/", github: "https://github.com/jmart6784/waldo")
 
-# Project.create(image: "cart.png", title: "Shopping Cart", description: "A shopping cart React app that uses React router", tags: "React, SCSS", live: "https://jmart6784.github.io/shopping-cart/", github: "https://github.com/jmart6784/shopping-cart")
+Project.create(image: "cart.png", title: "Shopping Cart", description: "A shopping cart React app that uses React router", tags: "React, SCSS", live: "https://jmart6784.github.io/shopping-cart/", github: "https://github.com/jmart6784/shopping-cart")
 
-# Project.create(image: "booker.png", title: "Odin Flight Booker", description: "A Flight booking app made using Rails", tags: "Rails, CSS", live: "https://odin-flight-booker365.herokuapp.com/", github: "https://github.com/jmart6784/odin-flight-booker")
+Project.create(image: "booker.png", title: "Odin Flight Booker", description: "A Flight booking app made using Rails", tags: "Rails, CSS", live: "https://odin-flight-booker365.herokuapp.com/", github: "https://github.com/jmart6784/odin-flight-booker")
 
-# Project.create(image: "instagram.png", title: "Instagram clone", description: "A full-stack Instagram clone app that uses Rails as the back-end. This project is also mobile friendly.", tags: "Rails, Javascipt, SCSS", live: "Soon", github: "https://github.com/jmart6784/instagram")
+Project.create(image: "instagram.png", title: "Instagram clone", description: "A full-stack Instagram clone app that uses Rails as the back-end. This project is also mobile friendly.", tags: "Rails, Javascipt, SCSS", live: "Soon", github: "https://github.com/jmart6784/instagram")
 
-# Project.create(image: "odinbook.png", title: "Odin Book", description: "My first attempt(Instgram clone is 2nd) at making a full stack application. I also implemnted omniauth support for Facebook sign in.", tags: "Rails, Javascript, CSS", live: "Soon", github: "https://github.com/jmart6784/odin-facebook")
+Project.create(image: "odinbook.png", title: "Odin Book", description: "My first attempt(Instgram clone is 2nd) at making a full stack application. I also implemnted omniauth support for Facebook sign in.", tags: "Rails, Javascript, CSS", live: "Soon", github: "https://github.com/jmart6784/odin-facebook")
 
-# Project.create(image: "battleship.png", title: "Battleship", description: "A Battleship game that was built for the purpose of practicing with Javascript and Jest.", tags: "Javascript, Jest, SCSS", live: "https://jmart6784.github.io/battleship/", github: "https://github.com/jmart6784/battleship")
+Project.create(image: "battleship.png", title: "Battleship", description: "A Battleship game that was built for the purpose of practicing with Javascript and Jest.", tags: "Javascript, Jest, SCSS", live: "https://jmart6784.github.io/battleship/", github: "https://github.com/jmart6784/battleship")
 
-# Project.create(image: "odinweather.png", title: "Odin Weather", description: "A weather app made for the purpose of asynchronous Javascript and React programming using a third party weather API.", tags: "React, Javascript, SCSS", live: "https://jmart6784.github.io/odin-weather/", github: "https://github.com/jmart6784/odin-weather")
+Project.create(image: "odinweather.png", title: "Odin Weather", description: "A weather app made for the purpose of asynchronous Javascript and React programming using a third party weather API.", tags: "React, Javascript, SCSS", live: "https://jmart6784.github.io/odin-weather/", github: "https://github.com/jmart6784/odin-weather")
 
-# Project.create(image: "memorycard.png", title: "Memory Card", description: "A React app that uses functional components and hooks.", tags: "React, SCSS", live: "https://jmart6784.github.io/memory-card/", github: "https://github.com/jmart6784/memory-card")
+Project.create(image: "memorycard.png", title: "Memory Card", description: "A React app that uses functional components and hooks.", tags: "React, SCSS", live: "https://jmart6784.github.io/memory-card/", github: "https://github.com/jmart6784/memory-card")
 
-# Project.create(image: "cv.png", title: "CV Project", description: "A React controlled components exercise app", tags: "React, SCSS", live: "https://jmart6784.github.io/cv-project/", github: "https://github.com/jmart6784/cv-project")
+Project.create(image: "cv.png", title: "CV Project", description: "A React controlled components exercise app", tags: "React, SCSS", live: "https://jmart6784.github.io/cv-project/", github: "https://github.com/jmart6784/cv-project")
 
-# Project.create(image: "todo.png", title: "Todo app", description: "Javascript based todo app used to practice factory functions and modules.", tags: "Javascript, SCSS", live: "https://jmart6784.github.io/odin-todo/", github: "https://github.com/jmart6784/odin-todo")
+Project.create(image: "todo.png", title: "Todo app", description: "Javascript based todo app used to practice factory functions and modules.", tags: "Javascript, SCSS", live: "https://jmart6784.github.io/odin-todo/", github: "https://github.com/jmart6784/odin-todo")
 
-# Project.create(image: "flickr.png", title: "Flickr Feed", description: "Practice using the Flickr API with Rails.", tags: "Rails", live: "https://odin-flickr.herokuapp.com/", github: "https://github.com/jmart6784/my-flickr-feed")
+Project.create(image: "flickr.png", title: "Flickr Feed", description: "Practice using the Flickr API with Rails.", tags: "Rails", live: "https://odin-flickr.herokuapp.com/", github: "https://github.com/jmart6784/my-flickr-feed")
 
-# Project.create(image: "dynamicui.png", title: "Dynamic interfaces", description: "Practice creating dynamic interfaces using Javascript", tags: "Javascript, SCSS", live: "https://jmart6784.github.io/dynamic-interfaces/", github: "https://github.com/jmart6784/dynamic-interfaces")
+Project.create(image: "dynamicui.png", title: "Dynamic interfaces", description: "Practice creating dynamic interfaces using Javascript", tags: "Javascript, SCSS", live: "https://jmart6784.github.io/dynamic-interfaces/", github: "https://github.com/jmart6784/dynamic-interfaces")
 
-# Project.create(image: "tictactoe.png", title: "Tic Tac Toe", description: "Tic Tac Toe game create for the purpose of practicing Javascript modules and factory functions.", tags: "Javascript, SCSS", live: "https://jmart6784.github.io/tic-tac-toe/", github: "https://github.com/jmart6784/tic-tac-toe")
+Project.create(image: "tictactoe.png", title: "Tic Tac Toe", description: "Tic Tac Toe game create for the purpose of practicing Javascript modules and factory functions.", tags: "Javascript, SCSS", live: "https://jmart6784.github.io/tic-tac-toe/", github: "https://github.com/jmart6784/tic-tac-toe")
 
-# Project.create(image: "library.png", title: "Odin Library", description: "A Library app to track which books you've read. The purpose was to practice Javascript objects and local storage.", tags: "Javascript, SCSS", live: "https://jmart6784.github.io/odin-library/", github: "https://github.com/jmart6784/odin-library")
+Project.create(image: "library.png", title: "Odin Library", description: "A Library app to track which books you've read. The purpose was to practice Javascript objects and local storage.", tags: "Javascript, SCSS", live: "https://jmart6784.github.io/odin-library/", github: "https://github.com/jmart6784/odin-library")
 
-# Project.create(image: "kittens.png", title: "Odin Kittens", description: "The purpose of this project was to practice creating a JSON API with rails.", tags: "Rails, Javascript, SCSS", live: "https://odin-kittens-api.herokuapp.com/", github: "https://github.com/jmart6784/odin-kittens")
+Project.create(image: "kittens.png", title: "Odin Kittens", description: "The purpose of this project was to practice creating a JSON API with rails.", tags: "Rails, Javascript, SCSS", live: "https://odin-kittens-api.herokuapp.com/", github: "https://github.com/jmart6784/odin-kittens")
 
-# Project.create(image: "restaurant.png", title: "Odin Restaurant", description: "The purpose of this project was to practice with webpacker and npm, exporting and importing files.", tags: "Javascript, SCSS", live: "https://jmart6784.github.io/odin-restaurant/", github: "https://github.com/jmart6784/odin-restaurant")
+Project.create(image: "restaurant.png", title: "Odin Restaurant", description: "The purpose of this project was to practice with webpacker and npm, exporting and importing files.", tags: "Javascript, SCSS", live: "https://jmart6784.github.io/odin-restaurant/", github: "https://github.com/jmart6784/odin-restaurant")
 
-# Project.create(image: "gridframework.png", title: "CSS grid Framework", description: "This project aims to build a simple grid framework.", tags: "CSS, SCSS", live: "https://jmart6784.github.io/grid-framework/", github: "https://github.com/jmart6784/grid-framework")
+Project.create(image: "gridframework.png", title: "CSS grid Framework", description: "This project aims to build a simple grid framework.", tags: "CSS, SCSS", live: "https://jmart6784.github.io/grid-framework/", github: "https://github.com/jmart6784/grid-framework")
 
-# Project.create(image: "responsivedesign.png", title: "Responsive Design", description: "Practice with responsive design using media queries.", tags: "CSS", live: "https://jmart6784.github.io/odin-responsive-design/", github: "https://github.com/jmart6784/odin-responsive-design")
+Project.create(image: "responsivedesign.png", title: "Responsive Design", description: "Practice with responsive design using media queries.", tags: "CSS", live: "https://jmart6784.github.io/odin-responsive-design/", github: "https://github.com/jmart6784/odin-responsive-design")
 
-# Project.create(image: "hangman.png", title: "Hangman", description: "A Sinatra based hangman game", tags: "Sinatra, CSS", live: "https://sinatra4567-hangman.herokuapp.com/", github: "https://github.com/jmart6784/sinatra-hangman")
+Project.create(image: "hangman.png", title: "Hangman", description: "A Sinatra based hangman game", tags: "Sinatra, CSS", live: "https://sinatra4567-hangman.herokuapp.com/", github: "https://github.com/jmart6784/sinatra-hangman")
 
-# Project.create(image: "jsvalidations.png", title: "JS form validations", description: "Simple Javascript form validation practice", tags: "Javascript", live: "https://jmart6784.github.io/js-form-validation/", github: "https://github.com/jmart6784/js-form-validation")
+Project.create(image: "jsvalidations.png", title: "JS form validations", description: "Simple Javascript form validation practice", tags: "Javascript", live: "https://jmart6784.github.io/js-form-validation/", github: "https://github.com/jmart6784/js-form-validation")
 
-# Project.create(image: "calculator.png", title: "Odin Calculator", description: "A simple calculator app built to practice with Javascript", tags: "Javascript, SCSS", live: "https://jmart6784.github.io/odin-calculator/", github: "https://github.com/jmart6784/odin-calculator")
+Project.create(image: "calculator.png", title: "Odin Calculator", description: "A simple calculator app built to practice with Javascript", tags: "Javascript, SCSS", live: "https://jmart6784.github.io/odin-calculator/", github: "https://github.com/jmart6784/odin-calculator")
 
-# Project.create(image: "bootstrap.png", title: "Bootstrap practice", description: "Practice using Bootstrap by cloning the Newsweek website", tags: "Bootstrap", live: "https://jmart6784.github.io/odin-bootstrap/", github: "https://github.com/jmart6784/odin-bootstrap")
+Project.create(image: "bootstrap.png", title: "Bootstrap practice", description: "Practice using Bootstrap by cloning the Newsweek website", tags: "Bootstrap", live: "https://jmart6784.github.io/odin-bootstrap/", github: "https://github.com/jmart6784/odin-bootstrap")
 
-# Project.create(image: "teardown.png", title: "Design teardown", description: "The purpose of this project was to practice color coding The Smashing Magazine's visual weight.", tags: "CSS", live: "https://jmart6784.github.io/odin-design-teardown/", github: "https://github.com/jmart6784/odin-design-teardown")
+Project.create(image: "teardown.png", title: "Design teardown", description: "The purpose of this project was to practice color coding The Smashing Magazine's visual weight.", tags: "CSS", live: "https://jmart6784.github.io/odin-design-teardown/", github: "https://github.com/jmart6784/odin-design-teardown")
 
-# Project.create(image: "apple.png", title: "Odin Apple", description: "The purpose of this project was to practice cloning a website.", tags: "CSS", live: "https://jmart6784.github.io/Odin-Apple/", github: "https://github.com/jmart6784/Odin-Apple")
+Project.create(image: "apple.png", title: "Odin Apple", description: "The purpose of this project was to practice cloning a website.", tags: "CSS", live: "https://jmart6784.github.io/Odin-Apple/", github: "https://github.com/jmart6784/Odin-Apple")
 
-# Project.create(image: "odintimes.png", title: "Odin Times", description: "Practice cloning a website (The NY Times).", tags: "CSS", live: "https://jmart6784.github.io/Odin-ny-times/", github: "https://github.com/jmart6784/Odin-ny-times")
+Project.create(image: "odintimes.png", title: "Odin Times", description: "Practice cloning a website (The NY Times).", tags: "CSS", live: "https://jmart6784.github.io/Odin-ny-times/", github: "https://github.com/jmart6784/Odin-ny-times")
 
-# Project.create(image: "cipher.png", title: "Ceaser's cipher", description: "Implmentation of Ceasar's cipher using Sinatra", tags: "Sinatra, CSS", live: "https://caesars-cipher4567.herokuapp.com/", github: "https://github.com/jmart6784/sinatra-cipher")
+Project.create(image: "cipher.png", title: "Ceaser's cipher", description: "Implmentation of Ceasar's cipher using Sinatra", tags: "Sinatra, CSS", live: "https://caesars-cipher4567.herokuapp.com/", github: "https://github.com/jmart6784/sinatra-cipher")
 
-# Project.create(image: "pomodoro.png", title: "Pomodoro Clock", description: "A Pomodoro clock made to practice programming with Javascript.", tags: "Javascript, CSS", live: "https://jmart6784.github.io/Pomodoro-Clock/", github: "https://github.com/jmart6784/Pomodoro-Clock")
+Project.create(image: "pomodoro.png", title: "Pomodoro Clock", description: "A Pomodoro clock made to practice programming with Javascript.", tags: "Javascript, CSS", live: "https://jmart6784.github.io/Pomodoro-Clock/", github: "https://github.com/jmart6784/Pomodoro-Clock")
 
-# Project.create(image: "etch.png", title: "Etch-a-sketch", description: "Etch-a-sketch app made to practice programming with Javascript.", tags: "Javascript, CSS", live: "https://jmart6784.github.io/Etch-a-sketch/", github: "https://github.com/jmart6784/Etch-a-sketch")
-
-# Project.create(image: ".png", title: "", description: "", tags: "", live: "", github: "")
+Project.create(image: "etch.png", title: "Etch-a-sketch", description: "Etch-a-sketch app made to practice programming with Javascript.", tags: "Javascript, CSS", live: "https://jmart6784.github.io/Etch-a-sketch/", github: "https://github.com/jmart6784/Etch-a-sketch")

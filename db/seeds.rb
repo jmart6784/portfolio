7c4f521163bded9fea0684396e76b594b754706e
jmart6784 <jmartinez6784@yahoo.com>
2021-02-07 22:20:21 -0800

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ Project.create(image: "booker.png", title: "Odin Flight Booker", description: "A
 
 Project.create(image: "instagram.png", title: "Instagram clone", description: "A full-stack Instagram clone app that uses Rails as the back-end. This project is also mobile friendly.", tags: "Rails, Javascript, SCSS", live: "https://cloned-instagram-app.herokuapp.com/", github: "https://github.com/jmart6784/instagram")
 
-Project.create(image: "odinbook.png", title: "Odin Book", description: "My first attempt(Instgram clone is 2nd) at making a full stack application. I also implemnted omniauth support for Facebook sign in.", tags: "Rails, Javascript, CSS", live: "Soon", github: "https://github.com/jmart6784/odin-facebook")
+Project.create(image: "odinbook.png", title: "Odin Book", description: "My first attempt(Instgram clone is 2nd) at making a full stack application. I also implemnted omniauth support for Facebook sign in.", tags: "Rails, Javascript, CSS", live: "https://odinbook-app.herokuapp.com", github: "https://github.com/jmart6784/odin-facebook")
 
 Project.create(image: "battleship.png", title: "Battleship", description: "A Battleship game that was built for the purpose of practicing with Javascript and Jest.", tags: "Javascript, Jest, SCSS", live: "https://jmart6784.github.io/battleship/", github: "https://github.com/jmart6784/battleship")
 
